3c803dae44798ebbbc90df7790b666fddf1f3f45
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-04-30 19:08:16 +0200

    test

--- app/Http/Controllers/CityMangerController.php
+++ app/Http/Controllers/CityMangerController.php
@@ -18,7 +18,7 @@ class CityMangerController extends Controller
             'name'=> $request_out['name'],
             'email'=> $request_out['email'],
             'password' => $request_out['password']
-            
+            //dddd
         ])->id;
         $CityManger=CityManger::create([
             'user_id'=>$user,
