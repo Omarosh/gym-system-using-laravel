01e0c2d2793cd2eed17d8c2748ac20a792897275
Isco <abdooisco24@gmail.com>
2022-05-04 17:30:41 +0200

    fixedNavbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,12 +16,13 @@
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css " rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
-    <script script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js">
+    <!-- <script script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js" defer></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" defer></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js" defer></script>
+    <script type="text/javascript"></script> -->
 
     @yield('third_party_stylesheets')
 
