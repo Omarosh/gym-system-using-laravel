46cfa64b3d12652d0429584dd5e14cdad17adcf8
MohamedYehia599 <mohamed.yehia3331@gmail.com>
2022-05-05 21:26:15 +0200

    edit trainee_package to be nullable

--- database/migrations/2022_05_04_172443_add_training_package_column_to_trainees_table.php
+++ database/migrations/2022_05_04_172443_add_training_package_column_to_trainees_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('trainees', function (Blueprint $table) {
-            // $table->unsignedBigInteger('training_package_id')->nullable();
-            $table->foreignId('training_package_id')->references('id')->on('trainingpackeges');
+            $table->unsignedBigInteger('training_package_id')->nullable();
+            $table->foreign('training_package_id')->references('id')->on('trainingpackeges');
 
         });
     }
