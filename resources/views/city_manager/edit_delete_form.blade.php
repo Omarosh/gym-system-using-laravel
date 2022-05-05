4a65563779c3ae35af6bb71d747c71066246cb77
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-05-05 18:08:57 +0200

    edited

--- resources/views/city_manager/edit_delete_form.blade.php
+++ resources/views/city_manager/edit_delete_form.blade.php
@@ -14,24 +14,12 @@
         <button type='submit' class='btn btn-info' style="margin-left: 10px;">Edit</button>
     </form>
 
-<<<<<<< HEAD
-    <button class='deletebutton btn btn-danger'>Delete</button>
-=======
         <button class='deletebutton{{$row->id}} btn btn-danger'>Delete</button>
->>>>>>> creating_trainees_tab
 </div>
 <script>
-    $(() => {
-        $("body").on("click", '.deletebutton{{$row->id}}', function() {
-            if (confirm('are you sure')) {
-                deleteCityManger($(this))
-            }
-        })
-    })
+   
 
 
-<<<<<<< HEAD
-=======
     $(()=>{
         $("body").on("click",'.deletebutton{{$row->id}}',function(){
             if( confirm('are you sure')){
@@ -39,39 +27,8 @@
             }
         })
     })
->>>>>>> creating_trainees_tab
 
-    function deleteCityManger(e) {
-        $(() => {
-            e.parent("div").parent("td").parent("tr").remove()
-            let id = Number(e.parent("div").parent("td").siblings("td").html())
-            console.log(id);
-
-            $.ajax({
-                type: "POST",
-                url: '/city_manager/delete',
-                data: {
-                    user_id: id,
-                    _token: '{{csrf_token()}}'
-                },
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function(data, textStatus, errorThrown) {
-                    console.log(data);
-
-                },
-            });
-
-<<<<<<< HEAD
-
-        })
-
-    }
-
-    })
-    }
-=======
+  
     function deleteCityManger(e){
         $(()=>{
             e.parent("div").parent("td").parent("tr").remove()
@@ -96,5 +53,4 @@
     }    
 
       
->>>>>>> creating_trainees_tab
 </script>
