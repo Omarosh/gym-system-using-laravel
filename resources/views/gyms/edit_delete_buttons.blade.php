11fd37213991f9d2be563c84dd63e080c2be9ab5
Isco <abdooisco24@gmail.com>
2022-05-06 17:56:33 +0200

    isco e

--- resources/views/gyms/edit_delete_buttons.blade.php
+++ resources/views/gyms/edit_delete_buttons.blade.php
@@ -1,8 +1,8 @@
 <div style="display: flex; justify-content:   space-around">
 
-<form method="POST" action="{{ route('gyms.view', $row->id) }}">
+    <form method="POST" action="{{ route('gyms.view', $row->id) }}">
         @csrf
-       
+
         <button type="submit" class="btn btn-info">View</button>
     </form>
 
@@ -22,34 +22,38 @@
 </div>
 
 <script>
-   
-    $(()=>{
-    $("body").on("click",".deletebutton{{$row->id}}",function(){
-        if( confirm('are you sure')){
-            deleteGym($(this))
-        }
-           })
+    $(() => {
+        $("body").on("click", ".deletebutton{{$row->id}}", function() {
+            if (confirm('are you sure')) {
+                deleteGym($(this))
+            }
+        })
     })
 
 
-    function deleteGym(e){
-        $(()=>{
-            e.parent("div").parent("td").parent("tr").remove()
-            let id=Number( e.parent("div").parent("td").siblings("td").html())
+    function deleteGym(e) {
+        $(() => {
+            let id = Number(e.parent("div").parent("td").siblings("td").html())
             console.log(id);
             $.ajax({
                 type: "POST",
                 url: "{{ route('gyms.delete') }}",
-                data: { id: id, _token: '{{csrf_token()}}' },
-                success: function (data) {
-                console.log(data);
+                data: {
+                    id: id,
+                    _token: '{{csrf_token()}}'
                 },
-                error: function (data, textStatus, errorThrown) {
+                success: function(data) {
+                    if (data = 'removed') {
+                        e.parent("div").parent("td").parent("tr").remove()
+                        console.log(data)
+                    } else alert('has sessions')
+                },
+                error: function(data, textStatus, errorThrown) {
                     console.log(data);
-            
+
                 },
             });
-                        
-        })   
+
+        })
     }
 </script>
