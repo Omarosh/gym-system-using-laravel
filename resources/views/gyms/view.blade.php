49dde5bdf955b2f7b0930f46acd6199c5fd73e04
Isco <abdooisco24@gmail.com>
2022-05-07 02:07:58 +0200

    hide column final

--- resources/views/gyms/view.blade.php
+++ resources/views/gyms/view.blade.php
@@ -14,7 +14,7 @@
             <th>Created at</th>
             <th>cover_image</th>
             <th>City Name</th>
-            @hasanyrole('city_manager')
+            @hasanyrole('admin')
 
             <th>City Manager Name</th>
             @endhasanyrole
@@ -34,7 +34,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js" defer></script>
 <script type="text/javascript"></script>
-@hasanyrole('city_manager')
+@hasanyrole('admin')
 
 <script>
    $(document).ready(function() {
@@ -81,7 +81,7 @@
 @endhasanyrole
 
 {{-- stRT cp --}}
-@hasanyrole('gym_manager|admin')
+@hasanyrole('gym_manager|city_manager')
 
 <script>
    $(document).ready(function() {
