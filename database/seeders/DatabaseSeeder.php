3afdc47a4457fecfb6ea33768f795447fd694510
Isco <abdooisco24@gmail.com>
2022-05-07 11:48:25 +0200

    add roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\CityManger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('roles')->insert([
+            'name'=>'admin',
+            'guard_name'=>'web'
+        ]);
+
+        DB::table('roles')->insert([
+            'name'=>'city_manager',
+            'guard_name'=>'web'
+        ]);
+
+        DB::table('roles')->insert([
+            'name'=>'gym_manager',
+            'guard_name'=>'web'
+        ]);
+
         $this->call([
             CityMangerSeeder::class,
             GymSeeder::class,
